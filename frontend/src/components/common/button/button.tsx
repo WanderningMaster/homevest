b925b6c3464c84b81e60e5e348d11d5625f52b6c
Natali-tk <natasha.totska@yahoo.com>
2022-02-03 21:57:29 +0200

    Remade Button component on TailwindCSS

--- frontend/src/components/common/button/button.tsx
+++ frontend/src/components/common/button/button.tsx
@@ -1,26 +1,45 @@
-import React from "react";
-import s from './button.module.scss'
-
+import React, { ReactNode } from "react";
+import "assets/styles/tailwind.css";
+import "assets/styles/fonts.css";
 interface ButtonProps {
-  nameBtn: "primary" | "secondary" | "tertiary";
-  children?: React.ReactNode;
+  label?: string;
+  icon?: ReactNode | HTMLAllCollection | string;
+  nameBtn?: "primary" | "secondary" | "tertiary" | "google";
+  disabled?: boolean;
+  children?: ReactNode;
+  type?: "button" | "submit" | "reset";
   onClick?: () => void;
+  [key: string]: any;
 }
 
+const STYLES = {
+  primary:
+    "text-white bg-green border-solid border-1 border-green hover:bg-green-light focus:bg-green-dark disabled:bg-green-inactive",
+  secondary:
+    "text-green border-solid border-1 border-green hover:border-green-light focus:border-green-dark disabled:border-green-inactive",
+  tertiary: "text-green border-none focus:text-green-dark ",
+  google: "text-green bg-white shadow-[0_4px_18px_rgba(0,0,0,0.1)]",
+};
+
 const Button: React.FC<ButtonProps> = ({
-  nameBtn,
+  label="",
+  nameBtn='primary',
+  disabled,
+  type="button",
   children,
   onClick,
   ...props
 }) => {
   return (
     <button
-      type="button"
+      type={type}
+      disabled={disabled}
       onClick={onClick}
-      className={`${s.button} ${s[nameBtn]}`}
+      className={`w-154 h-57 text-black font-medium text-base text-center bg-white box-border rounded-lg px-2 py-15 transition duration-200 ${STYLES[nameBtn]}`}
       {...props}
     >
       {children}
+      {label}
     </button>
   );
 };
