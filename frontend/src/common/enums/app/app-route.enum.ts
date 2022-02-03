d49ef699201f4d433c41bfa2ab77091d7e8f7a69
Olena Boldysheva <adv55577@gmail.com>
2022-02-04 00:31:51 +0200

    add testing routs for navigation

--- frontend/src/common/enums/app/app-route.enum.ts
+++ frontend/src/common/enums/app/app-route.enum.ts
@@ -2,6 +2,14 @@ enum AppRoute {
   ROOT = 'root',
   SIGN_IN = 'sign-in',
   SIGN_UP = 'sign-up',
+  HOME_PAGE = '/',
+  DASHBOARD_ONE = '/dashboard-1',
+  DASHBOARD_TWO = '/dashboard-2',
+  SETTINGS = '/settings',
+  MY_BUILDINGS = '/my-buildings',
+  MAP = '/map',
+  SEARCH = '/search',
+  PROPERTY = '/property',
 }
 
-export { AppRoute };
+export { AppRoute }
