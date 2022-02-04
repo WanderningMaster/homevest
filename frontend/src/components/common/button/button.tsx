131c4b6cd75b36fc9bd2f364418646c3f0ab7842
Natali-tk <natasha.totska@yahoo.com>
2022-02-04 21:45:25 +0200

    Made some fixing for Button component

--- frontend/src/components/common/button/button.tsx
+++ frontend/src/components/common/button/button.tsx
@@ -1,6 +1,8 @@
 import React, { ReactNode } from "react";
+import clsx from 'clsx';
 import "assets/styles/tailwind.css";
 import "assets/styles/fonts.css";
+import { ReactComponent as GoogleIcon } from "./icon-google.svg";
 interface ButtonProps {
   label?: string;
   icon?: ReactNode | HTMLAllCollection | string;
@@ -14,32 +16,38 @@ interface ButtonProps {
 
 const STYLES = {
   primary:
-    "text-white bg-green border-solid border-1 border-green hover:bg-green-hover focus:bg-green-pressed disabled:bg-green-inactive",
+    "text-white bg-green border-solid border border-green hover:bg-green-hover focus:bg-green-pressed disabled:bg-green-inactive",
   secondary:
-    "text-green border-solid border-1 border-green hover:border-green-hover focus:border-green-pressed disabled:border-green-inactive",
+    "text-green border-solid border border-green hover:border-green-hover focus:border-green-pressed disabled:border-green-inactive",
   tertiary: "text-green border-none focus:text-green-pressed ",
-  google: "text-green bg-white shadow-[0_4px_18px_rgba(0,0,0,0.1)]",
+  google: "text-green bg-white shadow-md",
 };
 
+
 const Button: React.FC<ButtonProps> = ({
-  label="",
-  nameBtn='primary',
+  className,
+  label = "",
+  nameBtn = "primary",
   disabled,
-  type="button",
+  type = "button",
   children,
+  icon,
   onClick,
   ...props
 }) => {
+  // const getIcon=nameBtn==="google"?GoogleIcon:
   return (
     <button
       type={type}
       disabled={disabled}
       onClick={onClick}
-      className={`w-154 h-57 text-black font-medium text-base text-center bg-white box-border rounded-lg px-2 py-15 transition duration-200 ${STYLES[nameBtn]}`}
+      className={clsx(
+        className, `flex justify-around items-center w-40 h-12 text-black font-medium text-base text-center bg-white box-border rounded-lg px-2 py-4 transition duration-200 ${STYLES[nameBtn]}`)}
       {...props}
     >
-      {children}
+      {nameBtn==="google"? <GoogleIcon className="w-5 h-5"/>:icon}
       {label}
+      {children}
     </button>
   );
 };
