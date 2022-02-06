e9c345d501120dfcd715e9e6b12ece974c703ae3
Olena Boldysheva <adv55577@gmail.com>
2022-02-06 11:50:38 +0200

    add props to Header

--- frontend/src/components/Navigation/Header/Header.tsx
+++ frontend/src/components/Navigation/Header/Header.tsx
@@ -1,13 +1,25 @@
 import * as React from 'react'
+import clsx from 'clsx'
 import { Logo } from 'components/Navigation/LogoLink'
 import { AppBar } from 'components/Navigation/AppBar'
-import SideBar from '../SideBar/SideBar'
+import { SideBar } from 'components/Navigation/SideBar'
 
-const Header: React.FC = () => {
+interface HederProps {
+  children?: React.ReactNode
+  position?: string
+}
+
+const Header: React.FC<HederProps> = ({ position, children }) => {
   return (
-    <header className="absolute min-w-full flex flex-row px justify-start items-center py-6 px-75px bg-white mx-0 my-0 shadow-header ">
-      <Logo />
-      <AppBar />
+    <header
+      className={clsx(
+        'absolute top-0 min-w-full flex flex-row px justify-start items-center py-6 px-75px bg-white mx-0 my-0 shadow-header ',
+        position,
+      )}
+    >
+      {children}
+      {/* <Logo /> */}
+      {/* <AppBar /> */}
     </header>
   )
 }
