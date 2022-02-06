da9f506d12bac16690b330225a3eb46c1c607ae1
Olena Boldysheva <adv55577@gmail.com>
2022-02-06 21:04:41 +0200

    Update padding

--- frontend/src/components/Navigation/Header/Header.tsx
+++ frontend/src/components/Navigation/Header/Header.tsx
@@ -10,7 +10,7 @@ const Header: React.FC<HederProps> = ({ position, children }) => {
   return (
     <header
       className={clsx(
-        'absolute top-0 min-w-full flex flex-row px justify-start items-center py-6 px-75px bg-white mx-0 my-0 shadow-header ',
+        'absolute top-0 min-w-full flex flex-row justify-start items-center py-8 px-75px bg-white mx-0 my-0 shadow-header ',
         position,
       )}
     >
