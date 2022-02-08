4cf7971e8a3d8939e5cfe2abc416f069b1191967
YuliaYassnyska <yuliayasso@gmail.com>
2022-02-08 17:32:48 +0200

    fix estate service

--- backend/src/services/estate-service/estate-service.service.ts
+++ backend/src/services/estate-service/estate-service.service.ts
@@ -1,20 +1,28 @@
-import { estateRepository } from '~/data/repositories';
+import { EstateRepository } from '~/data/repositories/estate-repository';
 import { IEstate} from '~/common/interfaces';
+import { EstateEntity } from '~/data/models/estate.entity';
+import { UpdateResult, DeleteResult, getCustomRepository } from "typeorm";
 
 class EstateService {
-  public getAllEstates():Promise<IEstate[]>{
-    return estateRepository.getAll()
+
+  public getAllEstates():Promise<EstateEntity[]>{
+    const estateRepository = getCustomRepository(EstateRepository);
+    return estateRepository.getAll();
   }
-  public getEstateById(id:string):Promise<IEstate | null>{
+  public getEstateById(id:string):Promise<EstateEntity | undefined>{
+    const estateRepository = getCustomRepository(EstateRepository);
     return estateRepository.getById(id)
   }
-  public createNewEstate(estate:IEstate):Promise<IEstate>{
-    return estateRepository.createEstate(estate)
+  public createNewEstate(estate:IEstate):Promise<EstateEntity>{
+    const estateRepository = getCustomRepository(EstateRepository);
+    return estateRepository.createEstate(estate);
   }
-  public async updateEstate(id:string, data:IEstate):Promise<IEstate[]>{
+  public async updateEstate(id:string, data: IEstate):Promise<UpdateResult>{
+    const estateRepository = getCustomRepository(EstateRepository);
     return estateRepository.updateById(id, data)
   }
-  public deleteEstate(id:string):Promise<number>{
+  public deleteEstate(id:string):Promise<DeleteResult>{
+    const estateRepository = getCustomRepository(EstateRepository);
     return estateRepository.deleteById(id)
   }
 }
