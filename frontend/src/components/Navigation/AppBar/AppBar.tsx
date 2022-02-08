2bd3216634433362062d5b824d06ab11866059ac
Olena Boldysheva <adv55577@gmail.com>
2022-02-08 14:44:50 +0200

    add typography & update header/navigationBar styles

--- frontend/src/components/Navigation/AppBar/AppBar.tsx
+++ frontend/src/components/Navigation/AppBar/AppBar.tsx
@@ -6,31 +6,36 @@ import { ReactComponent as MapIcon } from 'assets/images/map-icon.svg'
 import { ReactComponent as DeveloperIcon } from 'assets/images/developer-icon.svg'
 import { ReactComponent as SearchIcon } from 'assets/images/search-icon.svg'
 import { MenuItem } from 'components/Navigation/MenuItem'
+import Button from 'components/common/button/button'
 
 const AppBar: React.FC = () => {
   return (
-    <div className="static flex flex-row items-center p-0">
-      <Link to={AppRoute.NEW_BUILDINGS} className="text-black" activeClassName="text-green">
-        <MenuItem text="New buildings" itemStyles="mr-14 p-0" iconStyles="mr-2">
-          <CompoundIcon />
-        </MenuItem>
-      </Link>
-      <Link to={AppRoute.MAP} className="text-black" activeClassName="text-green">
-        <MenuItem text="Map" itemStyles="mr-14 p-0" iconStyles="mr-2">
-          <MapIcon />
-        </MenuItem>
-      </Link>
-      <Link to={AppRoute.PROPERTY} className="text-black" activeClassName="text-green">
-        <MenuItem text="Property developer" itemStyles="mr-14 p-0" iconStyles="mr-2">
-          <DeveloperIcon />
-        </MenuItem>
-      </Link>
-      <Link to={AppRoute.SEARCH} className="text-black" activeClassName="text-green">
-        <MenuItem text="Search" itemStyles="mr-14" iconStyles="mr-2">
-          <SearchIcon />
-        </MenuItem>
-      </Link>
-    </div>
+    <>
+      {' '}
+      <div className="static flex flex-row items-center p-0">
+        <Link to={AppRoute.NEW_BUILDINGS} className="text-black" activeClassName="text-green">
+          <MenuItem text="New buildings" itemStyles="mr-14" iconStyles="mr-2">
+            <CompoundIcon />
+          </MenuItem>
+        </Link>
+        <Link to={AppRoute.MAP} className="text-black" activeClassName="text-green">
+          <MenuItem text="Map" itemStyles="mr-14" iconStyles="mr-2">
+            <MapIcon />
+          </MenuItem>
+        </Link>
+        <Link to={AppRoute.PROPERTY} className="text-black" activeClassName="text-green">
+          <MenuItem text="Property developer" itemStyles="mr-14 p-0" iconStyles="mr-2">
+            <DeveloperIcon />
+          </MenuItem>
+        </Link>
+        <Link to={AppRoute.SEARCH} className="text-black" activeClassName="text-green">
+          <MenuItem text="Search" itemStyles="mr-14" iconStyles="mr-2">
+            <SearchIcon />
+          </MenuItem>
+        </Link>
+      </div>
+      <Button nameBtn="secondary" label="Log out" onClick={() => console.log('click')} />
+    </>
   )
 }
 
