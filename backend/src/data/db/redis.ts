30399c559e90a51fa86182f4190c2e48c3839486
WanderningMaster <56759981+WanderningMaster@users.noreply.github.com>
2022-02-09 11:46:17 +0200

    Update redis.ts

--- backend/src/data/db/redis.ts
+++ backend/src/data/db/redis.ts
@@ -8,11 +8,9 @@ const { REDIS_PORT } = process.env
 
 const redisCl = new Client();
 
-// const redisCl = new Client();
 async function connectRedis(): Promise<void> {
     if(!redisCl.isOpen()) {
         await redisCl.open(`redis://redis:${REDIS_PORT}`);
-        // await redisCl.open();
         logger.log("Redis is already plugged in");
     }
 }
