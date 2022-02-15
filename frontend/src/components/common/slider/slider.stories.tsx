8fde07dbab5268e4d73f7a0c45c54d985d346fad
BerezovskyiRuslan <berezovskyiruslanwork@gmail.com>
2022-02-15 04:25:49 +0200

    Created slider form default and card

--- frontend/src/components/common/slider/slider.stories.tsx
+++ frontend/src/components/common/slider/slider.stories.tsx
@@ -7,12 +7,20 @@ export default {
     component: Sliders
 } as ComponentMeta<typeof Sliders>;
 
-const Template: ComponentStory<typeof Sliders> = (args) => <Sliders {...args} />
+const Template: ComponentStory<typeof Sliders> = (args) => {
+    return <div className="w-1/2 h-300px"><Sliders {...args} /></div>
+}
 
 export const Empty = Template.bind({});
 
-export const Images = Template.bind({});
-Images.args = {
+export const Default = Template.bind({});
+Default.args = {
+    images: ["https://wallpaperaccess.com/full/1859246.jpg", "https://images.adsttc.com/media/images/5ecd/d4ac/b357/65c6/7300/009d/large_jpg/02C.jpg?1590547607"]
+}
+
+export const Card = Template.bind({});
+Card.args = {
+    type: 'card',
     images: ["https://wallpaperaccess.com/full/1859246.jpg", "https://images.adsttc.com/media/images/5ecd/d4ac/b357/65c6/7300/009d/large_jpg/02C.jpg?1590547607"]
 }
 
