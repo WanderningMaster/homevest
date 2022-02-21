9d081f05528de1a405af7a1331d6b9025bb5874d
Olena Boldysheva <adv55577@gmail.com>
2022-02-21 12:08:46 +0200

    update state structure for response from db

--- frontend/src/store/estate/estateReducer.tsx
+++ frontend/src/store/estate/estateReducer.tsx
@@ -1,7 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { ReducerName } from 'common/enums'
-import testImage from 'components/Dashboard/mock-data/image-2.png'
-import testImage2 from 'components/Dashboard/mock-data/image-3.png'
 
 export interface Actions {
   type: string
@@ -10,64 +8,48 @@ export interface Actions {
 
 export interface IEstate {
   id: string
-  name: string
-  image: string
+  estateId: string
+  nameOfBuilding: string
+  numberOfRooms: string
+  numberOfBathrooms: string
+  typeOfParking: string
+  priceForM2: string
   price: string
+  location: string
+  appartmentClass: string
+  floors: string
+  appartmentState: string
   currency: string
-  description: string
+  yearOfOperation: string
+  salesStatus: string
+  investmentType: string
+  lending: boolean
+  installments: boolean
+  mortgage: boolean
+  images: string
+  createdAt: string
+  updatedAt: string
 }
 
-const initialState: IEstate[] = [
-  {
-    id: '1',
-    name: 'RC Rainbow',
-    image: testImage,
-    price: '647 000',
-    currency: 'uah',
-    description: '1 is being built, 1 is build',
-  },
-  {
-    id: '2',
-    name: 'RC Rainbow',
-    image: testImage2,
-    price: '500 000',
-    currency: 'usd',
-    description: '1 is being built, 1 is build',
-  },
-  // {
-  //   id: '3',
-  //   name: 'RC Rainbow',
-  //   image: 'https://wallpaperaccess.com/full/1859246.jpg',
-  //   price: '647 000',
-  //   currency: 'uah',
-  //   description: '1 is being built, 1 is build',
-  // },
-  // {
-  //   id: '4',
-  //   name: 'RC Rainbow',
-  //   image: 'https://wallpaperaccess.com/full/1859246.jpg',
-  //   price: '500 000',
-  //   currency: 'usd',
-  //   description: '1 is being built, 1 is build',
-  // },
-]
+const initialState: IEstate[] = []
 
-// const setEstate = (): Actions => ({ type: 'SET_ESTATE' })
 const fetchEstate = (): Actions => ({ type: 'FETCH_ESTATE' })
 
 const { reducer, actions } = createSlice({
   name: ReducerName.ESTATE,
   initialState,
   reducers: {
-    setEstate: (state, action: PayloadAction<IEstate[]>) => {
-      return (state = action.payload)
+    setEstate: (_, action: PayloadAction<IEstate[]>) => {
+      console.log(action.payload)
+
+      return action.payload
     },
   },
 })
 
 const EstateActionsCreator = {
   ...actions,
-  //   setEstate,
+
   fetchEstate,
 }
 
---
+++ frontend/src/store/estate/operations.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import axios from 'axios'
+import { IEstate } from './estateReducer'
+
+axios.defaults.baseURL = 'http://localhost:3001/api/v1/'
+
+const fetchEstateFromApi = async () => {
+  try {
+    const data = await axios.get<IEstate[]>('/appartments')
+    console.log(data)
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export { fetchEstateFromApi }
